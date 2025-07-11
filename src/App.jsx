141707cb83108d6a192a9b47160cdec58b9df2b6
rudrabhairat09 <bhairatrudra@gmail.com>
2025-07-11 08:10:34 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 Growora Billing</h1>
+      <p>Welcome to your billing software!</p>
+    </div>
+  );
+}
+
+export default App;
